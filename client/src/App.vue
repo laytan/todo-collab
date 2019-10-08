cc540967736cb0e5f587bcff79b23d0e64da1fc0
laytan <Laytanlaats@hotmail.com>
2019-10-08 20:40:17 +0200

    Automatically re-authenticate if no user is found

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,24 @@
     <router-view/>
   </div>
 </template>
+<script>
+import { useState, useActions } from '@u3u/vue-hooks';
+import { onMounted } from '@vue/composition-api';
+import types from './types';
+
+export default {
+  setup() {
+    const { TRY_AUTH } = useActions([types.TRY_AUTH]);
+    const { user } = useState(['user']);
+
+    onMounted(async () => {
+      if (!user.id) {
+        TRY_AUTH();
+      }
+    });
+  },
+};
+</script>
 <style lang="scss">
 .red-border {
   background-color: rgba(0,0,0,0);
@@ -11,5 +29,21 @@
   &:hover, &:focus {
     background-color: #f44336;
   }
+}
+
+.flex {
+  display: flex;
+
+  &.between {
+    justify-content: space-between;
+  }
+
+  &.column {
+    flex-direction: column;
+  }
+}
+
+.m-0 {
+  margin: 0;
 }
 </style>
