11b097a6bdcffd96a141fcb6cb4932c7665affd8
laytan <Laytanlaats@hotmail.com>
2019-10-08 22:02:00 +0200

    add loading indicator

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <router-view/>
+    <div class="loading-spinner">
+      <i class="material-icons" v-if="loading">autorenew</i>
+    </div>
   </div>
 </template>
 <script>
@@ -11,17 +14,40 @@ import types from './types';
 export default {
   setup() {
     const { TRY_AUTH } = useActions([types.TRY_AUTH]);
-    const { user } = useState(['user']);
+    const { user, loading } = useState(['user', 'loading']);
 
     onMounted(async () => {
       if (!user.id) {
         TRY_AUTH();
       }
     });
+
+    return {
+      loading,
+    };
   },
 };
 </script>
 <style lang="scss">
+.loading-spinner {
+  position: fixed;
+  right: 10px;
+  bottom: 5px;
+
+  i {
+    animation: rotation 1s infinite linear;
+  }
+}
+
+@keyframes rotation {
+  from {
+      transform: rotate(0deg);
+  }
+  to {
+      transform: rotate(359deg);
+  }
+}
+
 .red-border {
   background-color: rgba(0,0,0,0);
   border: #f44336 2px solid;
