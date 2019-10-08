1dc776c062ebb261ea0f6ac5753a6fc6350b26ea
laytan <Laytanlaats@hotmail.com>
2019-10-08 23:21:46 +0200

    added author to todos on create

--- server/src/services/todos/todos.class.js
+++ server/src/services/todos/todos.class.js
@@ -1,5 +1,11 @@
 const { Service } = require('feathers-nedb');
 
 exports.Todos = class Todos extends Service {
-  
+
+  // Add authenticated user email as author and last editor
+  async create(params, { user }) {
+    params.author = user.email;
+    params.lastEditedBy = user.email;
+    return super.create(params);
+  }
 };
