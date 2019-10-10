a557c4a81370ad17982ac33b7770f629d585ed5d
laytan <Laytanlaats@hotmail.com>
2019-10-11 00:14:14 +0200

    Set up todolist channels

--- server/src/channels.js
+++ server/src/channels.js
@@ -1,61 +1,70 @@
-module.exports = function(app) {
-  if(typeof app.channel !== 'function') {
+module.exports = function (app) {
+  if (typeof app.channel !== 'function') {
     // If no real-time functionality has been configured just return
     return;
   }
 
-  app.on('connection', connection => {
-    // On a new real-time connection, add it to the anonymous channel
-    app.channel('anonymous').join(connection);
-  });
-
-  app.on('login', (authResult, { connection }) => {
+  app.on('login', async (authResult, { connection }) => {
     // connection can be undefined if there is no
     // real-time connection, e.g. when logging in via REST
-    if(connection) {
-      // Obtain the logged in user from the connection
-      // const user = connection.user;
-      
-      // The connection is no longer anonymous, remove it
-      app.channel('anonymous').leave(connection);
-
-      // Add it to the authenticated user channel
-      app.channel('authenticated').join(connection);
-
-      // Channels can be named anything and joined on any condition 
-      
-      // E.g. to send real-time events only to admins use
-      // if(user.isAdmin) { app.channel('admins').join(connection); }
-
-      // If the user has joined e.g. chat rooms
-      // if(Array.isArray(user.rooms)) user.rooms.forEach(room => app.channel(`rooms/${room.id}`).join(channel));
-      
-      // Easily organize users by email and userid for things like messaging
-      // app.channel(`emails/${user.email}`).join(channel);
-      // app.channel(`userIds/$(user.id}`).join(channel);
+    if (!connection) {
+      return;
     }
+
+    // Obtain the logged in user from the connection
+    const user = connection.user;
+
+    // Add it to the authenticated user channel
+    app.channel('authenticated').join(connection);
+
+    // find all todolists where the user is in the access array
+    const authorizedLists = await app.service('todolists').find({
+      query: {},
+      user
+    });
+
+    // add the user to the lists channel
+    authorizedLists.data.forEach(list => app.channel(`lists/${list._id}`).join(connection));
   });
 
-  // eslint-disable-next-line no-unused-vars
-  app.publish((data, hook) => {
-    // Here you can add event publishers to channels set up in `channels.js`
-    // To publish only for a specific event use `app.publish(eventname, () => {})`
+  app.on('logout', async (authResult, { connection }) => {
+    if (!connection) {
+      return;
+    }
 
-    console.log('Publishing all events to all authenticated users. See `channels.js` and https://docs.feathersjs.com/api/channels.html for more information.'); // eslint-disable-line
-
-    // e.g. to publish all service events to all authenticated users use
-    return app.channel('authenticated');
+    app.channel('authenticated').leave(connection);
   });
 
-  // Here you can also add service specific event publishers
-  // e.g. the publish the `users` service `created` event to the `admins` channel
-  // app.service('users').publish('created', () => app.channel('admins'));
-  
-  // With the userid and email organization from above you can easily select involved users
-  // app.service('messages').publish(() => {
-  //   return [
-  //     app.channel(`userIds/${data.createdBy}`),
-  //     app.channel(`emails/${data.recipientEmail}`)
-  //   ];
-  // });
+  // Publish all todolists service events only to its list channel
+  app.service('todolists').publish(list => {
+    return app.channel(`lists/${list._id}`);
+  });
+
+  // add all users in access to new lists channel
+  app.service('todolists').on('created', list => {
+    // Returns all current connections that have access to this list
+    const { connections } = app.channel(app.channels).filter(connection =>
+      list.access.includes(connection.user.email)
+    );
+
+    connections.map(conn => app.channel(`lists/${list._id}`).join(conn));
+  });
+
+  // Make all connections without access leave the channel
+  function onUpdateList(list) {
+    console.log('list updated, publishing');
+    app.channel(`lists/${list._id}`).leave(conn => !(list.access.includes(conn.user.email)));
+  }
+  app.service('todolists').on('updated', onUpdateList);
+  app.service('todolists').on('patched', onUpdateList);
+
+  // Make every connection leave this lists channel
+  app.service('todolists').on('removed', list => {
+    app.channel(`lists/${list._id}`).leave(() => true);
+  });
+
+  // Publish todo items to the parents list channel
+  app.service('todos').publish(todo => {
+    return app.channel(`lists/${todo.listId}`);
+  });
 };
