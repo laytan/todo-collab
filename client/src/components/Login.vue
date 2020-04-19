81cc674a2c52cddf50f7568ad710f6dd8d020fbf
Laytan Laats <Laytanlaats@hotmail.com>
2020-04-19 23:41:48 +0200

    change login to vue3 beta

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -2,44 +2,46 @@
   <div class="login">
     <h2>Login</h2>
     <form @submit.prevent="loginWithCreds">
-        <input type="email" placeholder="Email" v-model="state.email">
-        <input type="password" placeholder="Password" v-model="state.password">
-        <p class="red-text" v-if="state.error.length > 0">{{ state.error }}</p>
+        <input type="email" placeholder="Email" v-model="credentials.email">
+        <input type="password" placeholder="Password" v-model="credentials.password">
+        <p class="red-text" v-if="error.length > 0">{{ error }}</p>
         <input type="submit" value="login">
     </form>
   </div>
 </template>
 
 <script>
-import { reactive } from '@vue/composition-api';
-import { useActions } from '@u3u/vue-hooks';
+import { reactive, ref } from 'vue';
+import store from '../store';
 import types from '../types';
 
 export default {
   setup() {
-    const { LOGIN_WITH_CREDENTIALS } = useActions([types.LOGIN_WITH_CREDENTIALS]);
+    const { dispatch } = store;
 
-    const state = reactive({
+    const credentials = reactive({
       email: '',
       password: '',
-      error: '',
     });
 
-    async function loginWithCreds() {
-      state.error = '';
+    const error = ref('');
 
-      const errOrUser = await LOGIN_WITH_CREDENTIALS({
-        email: state.email,
-        password: state.password,
+    async function loginWithCreds() {
+      error.value = '';
+
+      const errOrUser = await dispatch(types.LOGIN_WITH_CREDENTIALS, {
+        email: credentials.email,
+        password: credentials.password,
       });
 
       if (errOrUser.error) {
-        state.error = errOrUser.error.message;
+        error.value = errOrUser.error.message;
       }
     }
 
     return {
-      state,
+      credentials,
+      error,
       loginWithCreds,
     };
   },
