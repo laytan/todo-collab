c722b473aa60e613970805ae5dadddd222e303d5
Laytan Laats <Laytanlaats@hotmail.com>
2020-04-20 00:26:18 +0200

    make name as vue3 compatible as possible

--- client/src/components/list/Name.vue
+++ client/src/components/list/Name.vue
@@ -4,19 +4,18 @@
       v-show="!editing"
       class="todolist-item-name"
       @click="openEditing"
-    >{{ name }}</div>
+    >{{ internalName }}</div>
     <div v-show="editing">
-      <input class="input-field inline" type="text" v-model="name" />
+      <input class="input-field inline" type="text" v-model="internalName" />
       <button @click="save" class="waves-effect waves-green green-text btn-flat">Save</button>
       <button @click="cancel" class="waves-effect waves-red red-text btn-flat">Cancel</button>
     </div>
   </div>
 </template>
 <script>
-import { ref } from '@vue/composition-api';
-import { useActions } from '@u3u/vue-hooks';
-import EventBus from '../../event-bus';
+import { ref } from 'vue';
 import types from '../../types';
+import store from '../../store';
 
 export default {
   props: {
@@ -27,30 +26,32 @@ export default {
     name: String,
   },
   setup(props) {
-    const { PATCH_ITEM } = useActions([types.PATCH_ITEM]);
+    const { dispatch } = store;
 
     const editing = ref(false);
+    const internalName = ref(props.name);
 
     function closeEditing() {
       setTimeout(() => {
         editing.value = false;
       }, 250);
-
-      EventBus.$emit('set-draggable', true);
+      // TODO: Rewrite event bus logic
+      // EventBus.$emit('set-draggable', true);
     }
-
-    EventBus.$on('close-editing', () => {
-      editing.value = false;
-    });
+    // TODO: Rewrite event bus logic
+    // EventBus.$on('close-editing', () => {
+    //   editing.value = false;
+    // });
 
     function openEditing() {
-      EventBus.$emit('close-editing');
-      EventBus.$emit('set-draggable', false);
+      // TODO: Rewrite event bus logic
+      // EventBus.$emit('close-editing');
+      // EventBus.$emit('set-draggable', false);
       editing.value = true;
     }
 
     function save() {
-      PATCH_ITEM({ id: props.todoId, patchData: { name: props.name } });
+      dispatch(types.PATCH_ITEM, { id: props.todoId, patchData: { name: internalName.value } });
       closeEditing();
     }
 
@@ -64,6 +65,7 @@ export default {
       save,
       cancel,
       openEditing,
+      internalName,
     };
   },
 };
