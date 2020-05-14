a6946b55ab53db74c7ad0f9076685bb0e7f0efdb
Laytan Laats <Laytanlaats@hotmail.com>
2020-05-14 23:47:03 +0200

    add non whitespace password

--- server/src/services/users/users.schema.js
+++ server/src/services/users/users.schema.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi');
 const usersSchema = Joi.object({
   username: Joi.string().min(2).max(25),
   email: Joi.string().email(),
-  password: Joi.string().min(6).max(200),
+  password: Joi.string().min(6).max(200).regex(/^\S*$/, 'password'),
 });
 
 module.exports = usersSchema;
