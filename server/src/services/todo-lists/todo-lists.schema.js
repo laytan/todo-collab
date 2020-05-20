afb5d6748a9ef5a86fef34ec99137eb9e1bc5902
Laytan Laats <Laytanlaats@hotmail.com>
2020-05-20 23:10:08 +0200

    allow empty list description

--- server/src/services/todo-lists/todo-lists.schema.js
+++ server/src/services/todo-lists/todo-lists.schema.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 
 const todoListsSchema = Joi.object({
   name: Joi.string().min(2).max(100),
-  description: Joi.string().max(500),
+  description: Joi.string().max(500).allow(''),
   password: Joi.string().min(6).max(200).regex(/^\S*$/, 'password'), // No whitespace
 });
 
