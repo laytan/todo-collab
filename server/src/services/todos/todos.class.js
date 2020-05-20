6c6d4c373d5b2a62ff18124215701f91f818f923
Laytan Laats <Laytanlaats@hotmail.com>
2020-05-20 22:37:26 +0200

    scope todos find to accessible lists

--- server/src/services/todos/todos.class.js
+++ server/src/services/todos/todos.class.js
@@ -1,10 +1,26 @@
 const { Service } = require('feathers-knex');
+const { getAccessibleLists } = require('../../helpers');
 
 exports.Todos = class Todos extends Service {
-  constructor(options) {
+  constructor(options, app) {
     super({
       ...options,
       name: 'todos',
     });
+
+    this.app = app;
+  }
+
+  async find(params) {
+    if(params.provider) {
+      const userId = params.user.id;
+  
+      // get lists the user has access to
+      const access = await getAccessibleLists(userId, this.app);
+      // query todos scoped to the lists that the user has access to on list_id
+      const todos = this.app.service('todos').find({ query: { ...params.query, list_id: { $in: access } } });
+      return todos;
+    }
+    return this._find(params);
   }
 };
