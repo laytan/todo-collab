2361146c53d05c8b212d958f8ce564b03120cd9c
Laytan Laats <Laytanlaats@hotmail.com>
2020-05-20 22:51:45 +0200

    forgot to pass app

--- server/src/services/todo-lists/todo-lists.class.js
+++ server/src/services/todo-lists/todo-lists.class.js
@@ -19,7 +19,7 @@ exports.TodoLists = class TodoLists extends Service {
     if (params.provider) {
       const userId = params.user.id;
       
-      const accessableListIds = await getAccessibleLists(userId);
+      const accessableListIds = await getAccessibleLists(userId, this.app);
 
       // Return the lists matching the user's query that the user has access to
       const query = { ...params.query, id: { $in: accessableListIds } };
